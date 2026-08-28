c14a96a02c8ea9e055b12836933212a2c3254536
bicho cutela <haydendanex@gmail.com>
2026-08-28 06:07:15 -0300

    Registra service worker de notificações no PWA

--- source/client/src/main.tsx
+++ source/client/src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/firebase-messaging-sw.js", { scope: "/" }).then((registration) => {
+      void registration.update();
+    }).catch(() => undefined);
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
