fe814deb7b2b7904b7b8aeff856f95046dae189b
bicho cutela <haydendanex@gmail.com>
2026-08-28 07:49:30 -0300

    Substitui rodapé do PWA pelo Sobre do Android

--- source/client/src/main.tsx
+++ source/client/src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import AboutFooter from "./components/AboutFooter";
 import "./index.css";
 import "./pages/PromotionsModal.css";
 import "./lib/barcodeEnhancer.css";
 import "./lib/barcodeEnhancer";
+import "./components/AboutFooter.css";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
@@ -14,3 +16,8 @@ if ("serviceWorker" in navigator) {
 }
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+const aboutRoot = document.createElement("div");
+aboutRoot.id = "nrd-about-root";
+document.getElementById("root")?.insertAdjacentElement("afterend", aboutRoot);
+createRoot(aboutRoot).render(<AboutFooter />);
