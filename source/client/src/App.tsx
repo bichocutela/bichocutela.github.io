8f54bb4b74bdc19911e6128800f24c567cb8342a
bicho cutela <haydendanex@gmail.com>
2026-09-13 15:02:58 -0300

    Exibe Consultar Produtos na navegação do PWA

--- source/client/src/App.tsx
+++ source/client/src/App.tsx
@@ -1,4 +1,5 @@
 /** NRD Lojas PWA: experiência mobile alinhada ao aplicativo Android. */
+import ProductConsultationEntry from "@/components/ProductConsultationEntry";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
@@ -25,6 +26,7 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
+          <ProductConsultationEntry />
           <Router />
         </TooltipProvider>
       </ThemeProvider>
